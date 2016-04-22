63dc99650e4911a6ba56620ad82054ec138c052c
Melody Kelly <melody@melody.blue>
2016-04-23 08:42:05 +1000

    Requirements and what you need to debug the HTML

--- tandr.py
+++ tandr.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,4 +10,4 @@ def login():
     return "ayy lmao"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
