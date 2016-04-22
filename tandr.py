0f857f94f0b5c9b0c95dd358ea232c6fb52dec12
Melody Kelly <melody@melody.blue>
2016-04-23 08:48:18 +1000

    Lets matt test pages

--- tandr.py
+++ tandr.py
@@ -3,11 +3,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html");
+    return render_template("index.html")
+
+@app.route("/rate")
+def rate():
+    return render_template("rate.html")
 
 @app.route("/login")
 def login():
-    return "ayy lmao"
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
