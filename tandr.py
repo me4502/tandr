648faab605ae7a99ed02372b04a0c1aa9b7f90e1
Melody Kelly <melody@melody.blue>
2016-04-23 10:06:28 +1000

    Testing shit

--- tandr.py
+++ tandr.py
@@ -1,5 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
+from flask_oauthlib.client import OAuth
 app = Flask(__name__)
+
+app.secret_key = "Ayy lmao"
+
+oauth_key="763519b5c8d1c478f44296c2b6c82dcb772dc4b0fbafa66b68d889cd41da4d71"
+oauth_secret="6ecb90487aaf53377f8a0e536c7a4a4ba3b142bb972d838b1034858bd5e670e5"
+
+tanda_url = "https://my.tanda.co/{}"
+oauth = OAuth()
+
+tanda_login = oauth.remote_app('tanda',
+        base_url=tanda_url.format(""),
+        request_token_url=None,
+        access_token_url=tanda_url.format("api/oauth/token"),
+        authorize_url=tanda_url.format("api/oauth/authorize"),
+        request_token_params={'scope':'me'},
+        consumer_key=oauth_key,
+        consumer_secret=oauth_secret)
 
 @app.route("/")
 def hello():
@@ -11,7 +29,12 @@ def rate():
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return tanda_login.authorize(callback=url_for('oauth_authorized',
+        next=request.args.get('next') or request.referrer or None))
+
+@app.route("/login/callback")
+def oauth_authorized():
+    return "Ayy lmao"
 
 if __name__ == "__main__":
     app.run(debug=True)
